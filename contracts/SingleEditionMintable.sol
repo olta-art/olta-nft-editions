a84d7995b5e32baf39505e7e6b9f57760eab233c
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-02-10 09:29:13 +0000

    feat: adds interface check

--- contracts/SingleEditionMintable.sol
+++ contracts/SingleEditionMintable.sol
@@ -336,6 +336,7 @@ contract SingleEditionMintable is
         returns (bool)
     {
         return
+            type(IEditionSingleMintable).interfaceId == interfaceId ||
             type(IERC2981Upgradeable).interfaceId == interfaceId ||
             ERC721Upgradeable.supportsInterface(interfaceId);
     }
