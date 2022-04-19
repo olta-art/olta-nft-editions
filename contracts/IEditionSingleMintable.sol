d562a4bb92069beebe16e1179a097430261886c2
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-04-19 13:04:03 +0100

    feat: update interface

--- contracts/IEditionSingleMintable.sol
+++ contracts/IEditionSingleMintable.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.6;
 
 interface IEditionSingleMintable {
   function mintEdition(address to) external returns (uint256);
+  function mintEdition(address to, uint256 seed) external returns (uint256);
   function mintEditions(address[] memory to) external returns (uint256);
+  function mintEditions(address[] memory to, uint256[] memory seed) external returns (uint256);
   function numberCanMint() external view returns (uint256);
   function owner() external view returns (address);
 }
