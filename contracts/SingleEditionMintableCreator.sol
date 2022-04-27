c5cd6d855cbe6345e191bf6c3c45b6463a8f0601
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-04-27 13:52:17 +0100

    refactor: use same functions for each implementation

--- contracts/SingleEditionMintableCreator.sol
+++ contracts/SingleEditionMintableCreator.sol
@@ -26,6 +26,17 @@ contract SingleEditionMintableCreator {
         seededEditions
     }
 
+    /// Important: None of these fields can be changed after calling
+    /// urls can be updated and upgraded via the versions interface
+    struct EditionData {
+        string name; // Name of the edition contract
+        string symbol; // Symbol of the edition contract
+        string description; /// Metadata: Description of the edition entry
+        Versions.Version version; /// Version media with animation url, animation sha256hash, image url, image sha256hash
+        uint256 editionSize; /// Total size of the edition (number of possible editions)
+        uint256 royaltyBPS; /// BPS amount of royalty
+    }
+
     /// Counter for current contract id upgraded
     CountersUpgradeable.Counter[2] private atContracts;
 
@@ -33,65 +44,72 @@ contract SingleEditionMintableCreator {
     address[2] public implementations;
 
     /// Initializes factory with address of implementations logic
-    /// @param _editionsImplementation SingleEditionMintable logic implementation contract to clone
-    /// @param _seededEditionsImplementation SeededSingleEditionMintable logic implementation contract to clone
-    constructor(address _editionsImplementation, address _seededEditionsImplementation) {
-        implementations[uint8(Implementation.editions)] = _editionsImplementation;
-        implementations[uint8(Implementation.seededEditions)] = _seededEditionsImplementation;
+    /// @param _implementations Array of addresse for implementations of SingleEditionMintable like contracts to clone
+    constructor(address[] memory _implementations) {
+        implementations[uint8(Implementation.editions)] = _implementations[uint8(Implementation.editions)];
+        implementations[uint8(Implementation.seededEditions)] = _implementations[uint8(Implementation.editions)];
     }
 
     /// Creates a new edition contract as a factory with a deterministic address
-    /// Important: None of these fields (except the Url fields with the same hash) can be changed after calling
-    /// @param _name Name of the edition contract
-    /// @param _symbol Symbol of the edition contract
-    /// @param _description Metadata: Description of the edition entry
-    /// @param _version Version media with animation url, animation sha256hash, image url, image sha256hash
-    /// @param _editionSize Total size of the edition (number of possible editions)
-    /// @param _royaltyBPS BPS amount of royalty
+    /// @param editionData EditionData of the edition contract
+    /// @param implementation Implementation of the edition contract
+
     function createEdition(
-        string memory _name,
-        string memory _symbol,
-        string memory _description,
-        Versions.Version memory _version,
-        uint256 _editionSize,
-        uint256 _royaltyBPS
+        EditionData memory editionData,
+        uint8 implementation
     ) external returns (uint256) {
-        uint256 newId = atContracts[uint8(Implementation.editions)].current();
+        uint256 newId = atContracts[implementation].current();
         address newContract = ClonesUpgradeable.cloneDeterministic(
-            implementations[uint8(Implementation.editions)],
+            implementations[implementation],
             bytes32(abi.encodePacked(newId))
         );
-        SingleEditionMintable(newContract).initialize(
-            msg.sender,
-            _name,
-            _symbol,
-            _description,
-            _version,
-            _editionSize,
-            _royaltyBPS
-        );
-        emit CreatedEdition(newId, msg.sender, _editionSize, newContract);
+
+        // Editions
+        if (implementation == uint8(Implementation.editions)){
+            SingleEditionMintable(newContract).initialize(
+                msg.sender,
+                editionData.name,
+                editionData.symbol,
+                editionData.description,
+                editionData.version,
+                editionData.editionSize,
+                editionData.royaltyBPS
+            );
+        }
+
+        // Seeded Editions
+        if (implementation == uint8(Implementation.seededEditions)){
+            SeededSingleEditionMintable(newContract).initialize(
+                msg.sender,
+                editionData.name,
+                editionData.symbol,
+                editionData.description,
+                editionData.version,
+                editionData.editionSize,
+                editionData.royaltyBPS
+            );
+        }
+
+        emit CreatedEdition(newId, msg.sender, editionData.editionSize, newContract, implementation);
         // Returns the ID of the recently created minting contract
         // Also increments for the next contract creation call
-        atContracts[uint8(Implementation.editions)].increment();
+        atContracts[implementation].increment();
         return newId;
     }
 
     /// Get edition given the created ID
     /// @param editionId id of edition to get contract for
     /// @return SingleEditionMintable Edition NFT contract
-    function getEditionAtId(uint256 editionId)
+    function getEditionAtId(uint256 editionId, uint8 implementation)
         external
         view
-        returns (SingleEditionMintable)
+        returns (address)
     {
         return
-            SingleEditionMintable(
-                ClonesUpgradeable.predictDeterministicAddress(
-                    implementations[uint8(Implementation.editions)],
-                    bytes32(abi.encodePacked(editionId)),
-                    address(this)
-                )
+            ClonesUpgradeable.predictDeterministicAddress(
+                implementations[implementation],
+                bytes32(abi.encodePacked(editionId)),
+                address(this)
             );
     }
 
@@ -101,70 +119,7 @@ contract SingleEditionMintableCreator {
         uint256 indexed editionId,
         address indexed creator,
         uint256 editionSize,
-        address editionContractAddress
-    );
-
-     /// Creates a new edition contract as a factory with a deterministic address
-    /// Important: None of these fields (except the Url fields with the same hash) can be changed after calling
-    /// @param _name Name of the edition contract
-    /// @param _symbol Symbol of the edition contract
-    /// @param _description Metadata: Description of the edition entry
-    /// @param _version Version media with animation url, animation sha256hash, image url, image sha256hash
-    /// @param _editionSize Total size of the edition (number of possible editions)
-    /// @param _royaltyBPS BPS amount of royalty
-    function createSeededEdition(
-        string memory _name,
-        string memory _symbol,
-        string memory _description,
-        Versions.Version memory _version,
-        uint256 _editionSize,
-        uint256 _royaltyBPS
-    ) external returns (uint256) {
-        uint256 newId = atContracts[uint8(Implementation.seededEditions)].current();
-        address newContract = ClonesUpgradeable.cloneDeterministic(
-            implementations[uint8(Implementation.seededEditions)],
-            bytes32(abi.encodePacked(newId))
-        );
-        SeededSingleEditionMintable(newContract).initialize(
-            msg.sender,
-            _name,
-            _symbol,
-            _description,
-            _version,
-            _editionSize,
-            _royaltyBPS
-        );
-        emit CreatedSeededEdition(newId, msg.sender, _editionSize, newContract);
-        // Returns the ID of the recently created minting contract
-        // Also increments for the next contract creation call
-        atContracts[uint8(Implementation.seededEditions)].increment();
-        return newId;
-    }
-
-    /// Get edition given the created ID
-    /// @param editionId id of edition to get contract for
-    /// @return SeededSingleEditionMintable Edition NFT contract
-    function getSeededEditionAtId(uint256 editionId)
-        external
-        view
-        returns (SeededSingleEditionMintable)
-    {
-        return
-            SeededSingleEditionMintable(
-                ClonesUpgradeable.predictDeterministicAddress(
-                    implementations[uint8(Implementation.seededEditions)],
-                    bytes32(abi.encodePacked(editionId)),
-                    address(this)
-                )
-            );
-    }
-
-    /// Emitted when a edition is created reserving the corresponding token IDs.
-    /// @param editionId ID of newly created edition
-    event CreatedSeededEdition(
-        uint256 indexed editionId,
-        address indexed creator,
-        uint256 editionSize,
-        address editionContractAddress
+        address editionContractAddress,
+        uint8 implementation
     );
 }
